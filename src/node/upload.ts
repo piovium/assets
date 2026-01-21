d60cec97c8d35cfdba0d9cbc33402b3a4189dd0f
Qiming Xu <33349132+xqm32@users.noreply.github.com>
2026-01-22 01:09:53 +0800

    refactor: use bun style

--- src/node/upload.ts
+++ src/node/upload.ts
@@ -1,18 +1,13 @@
 import { s3 } from "bun";
-import { readdir } from "node:fs/promises";
-import path from "node:path";
 import { publicDir } from "./config";
 import "./generate_thumbs";
 
-const entries = await readdir(publicDir, {
-  recursive: true,
-  withFileTypes: true,
-});
-const filenames = entries
-  .filter((entry) => entry.isFile())
-  .map((entry) => path.join(path.relative(publicDir, entry.path), entry.name));
-for (const filename of filenames) {
-  console.log(`Uploading ${filename}...`);
-  await s3.file(filename).write(Bun.file(path.join(publicDir, filename)));
-  console.log(`Uploaded ${filename}`);
+const glob = new Bun.Glob("**");
+for await (const path of glob.scan(publicDir)) {
+  const file = Bun.file(path);
+  if (file.type.startsWith("image/")) {
+    console.log("Uploading", path);
+    await s3.file(path).write(file);
+    console.log("Uploaded", path);
+  }
 }
