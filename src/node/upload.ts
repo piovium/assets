ef2819edeaef84a7dd32f8b61cc29718a36e3303
Qiming Xu <33349132+xqm32@users.noreply.github.com>
2026-01-22 01:14:42 +0800

    fix: join root dir

--- src/node/upload.ts
+++ src/node/upload.ts
@@ -1,10 +1,11 @@
 import { s3 } from "bun";
+import { join } from "node:path";
 import { publicDir } from "./config";
 import "./generate_thumbs";
 
 const glob = new Bun.Glob("**");
 for await (const path of glob.scan(publicDir)) {
-  const file = Bun.file(path);
+  const file = Bun.file(join(publicDir, path));
   if (file.type.startsWith("image/")) {
     console.log("Uploading", path);
     await s3.file(path).write(file);
