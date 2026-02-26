1f45e86897b451e06966c92b62c2c06a759db963
Qiming Xu <33349132+xqm32@users.noreply.github.com>
2026-02-26 17:12:44 +0800

    feat: delete old files

--- src/node/upload.ts
+++ src/node/upload.ts
@@ -3,12 +3,30 @@ import { join } from "node:path";
 import { publicDir } from "./config";
 import "./generate_thumbs";
 
+const uploads = [];
+let startAfter = undefined;
+while (true) {
+  const { isTruncated, contents } = await s3.list({ startAfter });
+  if (!isTruncated || !contents) break;
+  uploads.push(...contents);
+  startAfter = contents.at(-1)?.key;
+  if (!startAfter) break;
+}
+const uploadedPaths = new Set(uploads.map((upload) => upload.key));
+
 const glob = new Bun.Glob("**");
 for await (const path of glob.scan(publicDir)) {
   const file = Bun.file(join(publicDir, path));
   if (file.type.startsWith("image/")) {
     console.log("Uploading", path);
     await s3.file(path).write(file);
+    uploadedPaths.delete(path);
     console.log("Uploaded", path);
   }
+}
+
+for (const path of uploadedPaths) {
+  console.log("Deleting", path);
+  await s3.file(path).delete();
+  console.log("Deleted", path);
 }
